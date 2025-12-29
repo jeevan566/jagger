e51f960a652449cd8fbb006baf370fd32d330f74
jeevan566 <jeevan56jeevan56@gmail.com>
2025-12-29 18:07:15 +0000

    Update main.jsx

--- jagger-client/src/main.jsx
+++ jagger-client/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import 'bootstrap-icons/font/bootstrap-icons.css'; // ✅ THIS LINE
 import "./App.css";   // or "./App.css"
 
 
