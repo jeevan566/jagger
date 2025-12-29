caf52846313a5cc3510be7fbaf8aa7a42248c328
jeevan566 <jeevan56jeevan56@gmail.com>
2025-12-29 17:55:19 +0000

    Refactor routes and imports in App component

--- jagger-client/src/App.jsx
+++ jagger-client/src/App.jsx
@@ -1,9 +1,76 @@
+// import { Routes, Route } from "react-router-dom";
+// import Login from "./pages/Login";
+// import Dashboard from "./pages/Dashboard";
+// import Register from "./pages/Register";
+// import ProtectedRoute from "./components/ProtectedRoute";
+// import MainLayout from "./layout/MainLayout";
+// import Users from "./pages/Users";
+// import AddUser from "./pages/AddUser";
+// import Suppliers from "./pages/Suppliers";
+// import AddSupplier from "./pages/AddSupplier";
+// import Categories from "./pages/Categories";
+// import Products from "./pages/Products";
+// import AddProduct from "./pages/AddProduct";
+// import RFQs from "./pages/RFQs";
+// import CreateRFQ from "./pages/CreateRFQ";
+// import RFQDetails from "./pages/RFQDetails";
+// import SupplierRFQs from "./pages/SupplierRFQs";
+// import SubmitQuote from "./pages/SubmitQuote";
+// import CompareQuotes from "./pages/CompareQuotes";
+// import POList from "./pages/POList";
+// import PODetails from "./pages/PODetails";
+// import CreatePO from "./pages/CreatePO";
+// import AuditLogs from "./pages/AuditLogs";
+// import AdminSettings from "./pages/AdminSettings";
+// import Inventory from "./pages/Inventory"
+
+
+// export default function App() {
+//   return (
+//     <Routes>
+//       {/* Public */}
+//       <Route path="/login" element={<Login />} />
+
+//       {/* Protected Layout */}
+//       <Route
+//         path="/"
+//         element={
+//           <ProtectedRoute>
+//             <MainLayout />
+//           </ProtectedRoute>
+//         }
+//       >
+//         <Route path="dashboard" element={<Dashboard />} />
+//       </Route>
+//         <Route path="register" element={<Register />} />
+//       <Route path="users" element={<Users />} />
+//       <Route path="*" element={<Login />} />
+//       <Route path="add-user" element={<AddUser />} />
+//       <Route path="suppliers" element={<Suppliers />} />
+//       <Route path="add-supplier" element={<AddSupplier />} />
+//       <Route path="categories" element={<Categories />} />
+//       <Route path="products" element={<Products />} />
+//       <Route path="add-product" element={<AddProduct />} />
+//       <Route path="rfqs" element={<RFQs />} />
+//       <Route path="create-rfq" element={<CreateRFQ />} />
+//       <Route path="rfq/:id" element={<RFQDetails />} />
+//       <Route path="supplier-rfqs" element={<SupplierRFQs />} />
+//       <Route path="submit-quote/:id" element={<SubmitQuote />} />
+//       <Route path="rfq/:id/compare" element={<CompareQuotes />} />
+//       <Route path="pos" element={<POList />} />
+//       <Route path="po/:id" element={<PODetails />} />
+//       <Route path="create-po/:id" element={<CreatePO />} />
+//       <Route path="audit" element={<AuditLogs />} />
+//       <Route path="settings" element={<AdminSettings />} />
+//       <Route path="/inventory" element={<Inventory />} />
+
+//     </Routes>
+//   );
+// }
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 import Register from "./pages/Register";
-import ProtectedRoute from "./components/ProtectedRoute";
-import MainLayout from "./layout/MainLayout";
+import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import AddUser from "./pages/AddUser";
 import Suppliers from "./pages/Suppliers";
@@ -22,16 +89,18 @@ import PODetails from "./pages/PODetails";
 import CreatePO from "./pages/CreatePO";
 import AuditLogs from "./pages/AuditLogs";
 import AdminSettings from "./pages/AdminSettings";
-import Inventory from "./pages/Inventory"
-
+import Inventory from "./pages/Inventory";
+import ProtectedRoute from "./components/ProtectedRoute";
+import MainLayout from "./layout/MainLayout";
 
 export default function App() {
   return (
     <Routes>
-      {/* Public */}
+      {/* Public routes (NO sidebar) */}
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
 
-      {/* Protected Layout */}
+      {/* Protected routes with Layout */}
       <Route
         path="/"
         element={
@@ -41,29 +110,36 @@ export default function App() {
         }
       >
         <Route path="dashboard" element={<Dashboard />} />
-      </Route>
-        <Route path="register" element={<Register />} />
-      <Route path="users" element={<Users />} />
-      <Route path="*" element={<Login />} />
-      <Route path="add-user" element={<AddUser />} />
-      <Route path="suppliers" element={<Suppliers />} />
-      <Route path="add-supplier" element={<AddSupplier />} />
-      <Route path="categories" element={<Categories />} />
-      <Route path="products" element={<Products />} />
-      <Route path="add-product" element={<AddProduct />} />
-      <Route path="rfqs" element={<RFQs />} />
-      <Route path="create-rfq" element={<CreateRFQ />} />
-      <Route path="rfq/:id" element={<RFQDetails />} />
-      <Route path="supplier-rfqs" element={<SupplierRFQs />} />
-      <Route path="submit-quote/:id" element={<SubmitQuote />} />
-      <Route path="rfq/:id/compare" element={<CompareQuotes />} />
-      <Route path="pos" element={<POList />} />
-      <Route path="po/:id" element={<PODetails />} />
-      <Route path="create-po/:id" element={<CreatePO />} />
-      <Route path="audit" element={<AuditLogs />} />
-      <Route path="settings" element={<AdminSettings />} />
-      <Route path="/inventory" element={<Inventory />} />
+        <Route path="users" element={<Users />} />
+        <Route path="add-user" element={<AddUser />} />
 
+        <Route path="suppliers" element={<Suppliers />} />
+        <Route path="add-supplier" element={<AddSupplier />} />
+
+        <Route path="inventory" element={<Inventory />} />
+
+        <Route path="categories" element={<Categories />} />
+        <Route path="products" element={<Products />} />
+        <Route path="add-product" element={<AddProduct />} />
+
+        <Route path="rfqs" element={<RFQs />} />
+        <Route path="create-rfq" element={<CreateRFQ />} />
+        <Route path="rfq/:id" element={<RFQDetails />} />
+        <Route path="rfq/:id/compare" element={<CompareQuotes />} />
+
+        <Route path="supplier-rfqs" element={<SupplierRFQs />} />
+        <Route path="submit-quote/:id" element={<SubmitQuote />} />
+
+        <Route path="pos" element={<POList />} />
+        <Route path="po/:id" element={<PODetails />} />
+        <Route path="create-po/:id" element={<CreatePO />} />
+
+        <Route path="audit" element={<AuditLogs />} />
+        <Route path="settings" element={<AdminSettings />} />
+      </Route>
+
+      {/* Fallback */}
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
